245db2c670b6d0fa9a306d63f38b666f9dea87c4
Exmane <ex.mane.orig@gmail.com>
2024-09-28 23:49:40 +0200

    upload

---
+++ Android.mk
@@ -0,0 +1,19 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),vili)
+
+$(info Including firmware for vili...)
+
+FIRMWARE_IMAGES := $(wildcard $(LOCAL_PATH)/images/*)
+
+$(foreach f, $(notdir $(FIRMWARE_IMAGES)), \
+    $(call add-radio-file,images/$(f)))
+
+endif
+
---
+++ BoardConfigVendor.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+FIRMWARE_IMAGES := $(wildcard vendor/xiaomi/vili-firmware/images/*)
+
+AB_OTA_PARTITIONS += \
+    $(foreach f, $(notdir $(FIRMWARE_IMAGES)), $(basename $(f)))
+
